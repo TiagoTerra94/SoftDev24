fd08912d8f041c0dd380fd6df3f51d8a4a5d3848
TiagoTerra94 <coio_terra@hotmail.com>
2024-12-28 13:51:28 +0000

    Last Update

---
+++ 03.Algoritmos e Programação/TP_GrupoIII/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//Grupo III do Trabalho Prático
+//Estatisticas
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    static int op =1;
+    public static void main(String[] args) {
+
+
+        do{
+            System.out.println("RESULTADOS ATO ELEITORAL");
+
+
+        }while(op!=0);
+
+    }
+}
