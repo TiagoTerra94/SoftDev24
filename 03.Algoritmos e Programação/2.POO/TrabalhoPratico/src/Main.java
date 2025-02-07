4355e8a28960cbe53308a50959986e5495fd97fe
TiagoTerra94 <coio_terra@hotmail.com>
2025-02-07 16:39:59 +0000

    Main update

--- 03.Algoritmos e Programação/2.POO/TrabalhoPratico/src/Main.java
+++ 03.Algoritmos e Programação/2.POO/TrabalhoPratico/src/Main.java
@@ -1,9 +1,13 @@
 import Main_Entity.Hero;
+import Main_Entity.NPC;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Hero crea
+        Game game1 = new Game();
+        Hero hero = game1.createCharacter();
+        System.out.println("You have chosen: " + hero.getClass().getSimpleName());
     }
+
 }
