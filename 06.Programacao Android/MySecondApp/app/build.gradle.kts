1334cec96e9e359ae5a5dfba75542ac94d2fa628
TiagoTerra94 <coio_terra@hotmail.com>
2025-02-21 15:56:11 +0000

    Android Update

--- 06.Programacao Android/MySecondApp/app/build.gradle.kts
+++ 06.Programacao Android/MySecondApp/app/build.gradle.kts
@@ -48,9 +48,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.play.services.ads.lite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.gms:play-services-ads:23.6.0")
+
 }
